eef0758f09543c73985acdd6cb504c3bd744be5b
Ygor Goulart <ygorgm95@gmail.com>
2022-12-21 16:19:48 -0300

    add todo to list

---
+++ index.js
@@ -0,0 +1,11 @@
+const input = document.getElementById("todo-input");
+const addButton = document.getElementById("add-button");
+const list = document.getElementById("todo-list");
+
+addButton.addEventListener("click", () => {
+  const todo = input.value;
+  input.value = "";
+  const item = document.createElement("div");
+  itme.innerHTML = todo;
+  list.appendChild(item);
+});
