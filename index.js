0c1f7137343ffca0810e9531551e1626a9403813
Ygor Goulart <ygorgm95@gmail.com>
2022-12-21 16:31:29 -0300

    refactor event listener

--- index.js
+++ index.js
@@ -2,10 +2,31 @@ const input = document.getElementById("todo-input");
 const addButton = document.getElementById("add-button");
 const list = document.getElementById("todo-list");
 
+function createCheckbox(item) {
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.addEventListener("click", () => {
+    item.classList.toggle("completed");
+  });
+  item.appendChild(checkbox);
+}
+
+function clearTextInput() {
+  input.value = "";
+}
+
+function createTodo(todo) {
+  item = document.createElement("div");
+  createCheckbox(item);
+  item.appendChild(document.createTextNode(todo));
+  return item;
+}
+
 addButton.addEventListener("click", () => {
   const todo = input.value;
-  input.value = "";
-  const item = document.createElement("div");
-  item.innerHTML = todo;
-  list.appendChild(item);
+
+  if (todo !== "") {
+    clearTextInput();
+    list.appendChild(createTodo(todo));
+  }
 });
