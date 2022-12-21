f529087c3880896c27753ab3a3384b446f382c44
Ygor Goulart <ygorgm95@gmail.com>
2022-12-21 18:27:12 -0300

    feat add persistent checkmark, array system and clear array

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const input = document.getElementById("todo-input");
 const addButton = document.getElementById("add-button");
 const clearButton = document.getElementById("clear-list");
 const list = document.getElementById("todo-list");
+const todoList = ["item 1", "item2", "item3"];
+const checkState = {};
 
 // clear input text in textbox
 function clearTextInput() {
@@ -10,22 +12,32 @@ function clearTextInput() {
 
 // create to-do item
 function createTodo(todo) {
-  item = document.createElement("div");
-  createCheckbox(item);
+  const item = document.createElement("div");
   item.classList.add("todo");
+
+  item.appendChild(createCheckbox(todo, item));
   item.appendChild(document.createTextNode(todo));
   return item;
 }
 
 // create checkbox item to use in each to-do item
-function createCheckbox(item) {
+function createCheckbox(todo, item) {
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
+  checkbox.checked = checkState[todo] || false; // Set the check state
+
+  checkbox.addEventListener("change", function () {
+    checkState[todo] = this.checked; // Update the check state in the object
+  });
+
+  // add click to item
   item.addEventListener("click", () => {
     item.classList.toggle("completed");
     checkbox.checked = !checkbox.checked;
+    checkState[todo] = checkbox.checked;
   });
-  item.appendChild(checkbox);
+
+  return checkbox;
 }
 
 // add to-do item to list
@@ -33,6 +45,16 @@ function addToList() {
   const todo = input.value;
   if (todo !== "") {
     clearTextInput();
+    todoList.push(todo);
+    updateList();
+  }
+}
+
+// update todo list
+function updateList() {
+  clearList();
+  // Add the new items to the list
+  for (const todo of todoList) {
     list.appendChild(createTodo(todo));
   }
 }
@@ -54,16 +76,25 @@ clearButton.addEventListener("click", () => {
   if (list.firstChild !== null) {
     if (confirm("Are you sure you want to clear the list?")) {
       // user clicked "OK"
-      clearList();
-    } else {
-      // user clicked "Cancel"
+      clearTodoList();
+      updateList();
     }
   }
 });
 
-// clear list
+// clear list (HTML element)
 function clearList() {
   while (list.firstChild) {
     list.removeChild(list.firstChild);
   }
 }
+
+// clear list (array)
+function clearTodoList() {
+  todoList.length = 0;
+}
+
+// executes when the page load
+window.addEventListener("load", () => {
+  updateList();
+});
